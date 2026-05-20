e545f6ab5085f9b22c7e55380c25714b4ac8a5a5
Paul Angeles <paul.angeles.chaquire@gmail.com>
2026-05-20 16:27:50 +0200

    Fix i18n store compliance, MC parser bug, and clean code guidelines

--- raycast-env.d.ts
+++ raycast-env.d.ts
@@ -7,10 +7,7 @@
 
 /* eslint-disable @typescript-eslint/ban-types */
 
-type ExtensionPreferences = {
-  /** Language - Language of the extension (affects syntax for correct/true) */
-  "language": "de" | "en" | "es" | "zh" | "hi" | "ru" | "ar" | "pt" | "it" | "tr"
-}
+type ExtensionPreferences = {}
 
 /** Preferences accessible in all the extension's commands */
 declare type Preferences = ExtensionPreferences
